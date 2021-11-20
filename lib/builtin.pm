3e15a5cdc31d9e3725e98a3e8a6d44ba972cf6cd
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2021-11-20 22:02:47 +0000

    An initial attempt at some module docs

--- lib/builtin.pm
+++ lib/builtin.pm
@@ -1,5 +1,3 @@
-# TODO: Some (C) header
-
 package builtin;
 
 use strict;
@@ -22,6 +20,76 @@ builtin - Perl pragma to import built-in utility functions
 
 =head1 DESCRIPTION
 
-TODO
+Perl provides several utility functions in the C<builtin> package.
+
+Functions in this package can be called by their fully-qualified names, but
+it is often desired to give them shorter names for convenience. Individual
+named functions can be imported by listing them as import parameters on the
+C<use> statement for this pragma.
+
+=head2 Lexical Import
+
+This pragma module creates I<lexical> aliases in the currently-compiling scope
+to these builtin functions. This is similar to the lexical effect of other
+pragmas such as L<strict> and L<feature>.
+
+    sub classify
+    {
+        my $sv = shift;
+
+        use builtin 'isbool';
+        return isbool($sv) ? "boolean" : "not a boolean";
+    }
+
+    # the isbool() function is no longer visible here
+    # but may still be called by builtin::isbool()
+
+Because these functions are imported lexically, rather than by package
+symbols, the user does not need to take any special measures to ensure they
+don't accidentally appear as object methods from a class.
+
+    package An::Object::Class {
+        use builtin 'true', 'false';
+        ...
+    }
+
+    # does not appear as a method
+    An::Object::Class->true;
+
+    # Can't locate object method "true" via package "An::Object::Class"
+    #   at ...
+
+=head1 FUNCTIONS
+
+=head2 true
+
+    $val = true;
+
+Returns the boolean truth value. While any scalar value can be tested for
+truth and most defined, non-empty and non-zero values are considered "true"
+by perl, this one is special in that L</isbool> considers it to be a
+distinguished boolean value.
+
+=head2 false
+
+    $val = false;
+
+Returns the boolean fiction value. While any non-true scalar value is
+considered "false" by perl, this one is special in that L</isbool> considers
+it to be a distinguished boolean value.
+
+=head2 isbool
+
+    $bool = isbool($val);
+
+Returns true when given a distinguished boolean value, or false if not. A
+distinguished boolean value is the result of any boolean-returning builtin
+function (such as C<true> or C<isbool> itself), boolean-returning operator
+(such as the C<eq> or C<==> comparison tests or the C<!> negation operator),
+or any variable containing one of these results.
+
+=head1 SEE ALSO
+
+L<perlop>, L<perlfunc>, L<Scalar::Util>
 
 =cut

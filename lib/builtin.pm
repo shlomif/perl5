2fd76b851e41fad6bc6f54b2ba5a4dcc228318fe
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2021-11-20 16:40:59 +0000

    A very initial hack at adding a builtin:: namespace, with true/false/isbool

---
+++ lib/builtin.pm
@@ -0,0 +1,27 @@
+# TODO: Some (C) header
+
+package builtin;
+
+use strict;
+use warnings;
+
+# All code, including &import, is implemented by always-present functions in
+# the perl interpreter itself.
+# See also `builtin.c` in perl source
+
+1;
+__END__
+
+=head1 NAME
+
+builtin - Perl pragma to import built-in utility functions
+
+=head1 SYNOPSIS
+
+    use builtin qw( true false isbool );
+
+=head1 DESCRIPTION
+
+TODO
+
+=cut
